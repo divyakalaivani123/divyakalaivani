388de0171bc7c1a126074f9f9fb33a42c2f7abdc
divyakalaivani123 <divyakalaivani.joy@gmail.com>
2024-10-22 00:40:08 +0530

    initial import

---
+++ example1/src/Main.java
@@ -0,0 +1,20 @@
+class example
+{
+    public static void main(String []args)
+    {
+        System.out.println("my first java program");
+        int x = 10;
+        int y = 7;
+
+        int sum = x+y;
+        float div = (float)x/y;
+        int sub = x-y;
+        int mul = x*y;
+
+        System.out.println("ADDITION OF TWO NUMBERS IS:" +sum);
+        System.out.println("DIVISION OF TWO NUMBERS IS:" +div);
+        System.out.println("SUBTRACTION OF TWO NUMBERS IS:" +sub);
+        System.out.println("MULTIPLICATION OF TWO NUMBERS IS:" +mul);
+
+    }
+}
